061b82d98400e2a3bc10dc6472ed8f669a4bca15
Junho Kim <junho.kim@wob-racing.de>
2024-02-13 13:12:25 -0500

    Update to passthrough cloud near by lidar

--- src/lidar/src/lidar_sub.cpp
+++ src/lidar/src/lidar_sub.cpp
@@ -7,6 +7,7 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -37,22 +38,46 @@ private:
     pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*cloud_msg, *pointcloud);
 
+    //Filtering
+    pcl::PointCloud<pcl::PointXYZ>::Ptr ptr_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr center(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr x_out(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr y_out(new pcl::PointCloud<pcl::PointXYZ>);
+    
     // Voxelization
     pcl::PointCloud<pcl::PointXYZ> pc_voxelized;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr ptr_filtered(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::VoxelGrid<pcl::PointXYZ> voxel_filter;
-    float voxelsize = 0.2;
+    float voxelsize = 0.2; // m
     voxel_filter.setInputCloud(pointcloud);
     voxel_filter.setLeafSize(voxelsize, voxelsize, voxelsize);
     voxel_filter.filter(*ptr_filtered);
-    pc_voxelized = *ptr_filtered;
-    // Pass Through Filter
+    // pc_voxelized = *ptr_filtered;
 
+    // Pass Through Filter
+    float x_offset = 1.5;
+    float y_offset = 1.5;
+    pcl::PassThrough<pcl::PointXYZ> xfilter;
+    xfilter.setInputCloud(ptr_filtered);
+    xfilter.setFilterFieldName("x");
+    xfilter.setFilterLimits(-x_offset, x_offset);
+    xfilter.filter(*center);
+    xfilter.setFilterLimitsNegative(true);
+    xfilter.filter(*x_out);
+
+    pcl::PassThrough<pcl::PointXYZ> yfilter;
+    yfilter.setInputCloud(center);
+    yfilter.setFilterFieldName("y");
+    yfilter.setFilterLimits(-y_offset, y_offset);
+    yfilter.setFilterLimitsNegative(true);
+    yfilter.filter(*y_out);
+
+    *y_out += *x_out;
+    
     // Ground Remover
 
     // publish processed lidar
     sensor_msgs::msg::PointCloud2 cloudmsg;
-    pcl::toROSMsg(pc_voxelized, cloudmsg);
+    pcl::toROSMsg(*y_out, cloudmsg);
     cloudmsg.header.frame_id = "laser_data_frame";
     cloudmsg.header.stamp = this->get_clock()->now();
     pointcloud_publisher->publish(cloudmsg);
